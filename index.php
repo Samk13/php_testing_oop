e0f68c22459b1d74ee195ac7e04aea417691d348
Samk13 <samarbid13@gmail.com>
2020-10-09 15:45:24 +0200

    Booleans

--- index.php
+++ index.php
@@ -1,56 +1,10 @@
 <?php
 
-class Team
-{
-    /**
-     *  @param $name
-     * @param $members
-     *
-     */
-    protected $name;
-    protected $members;
-    public function __construct($name, $members = [])
-    {
-        $this->name = $name;
-        $this->members = $members;
-    }
+$task =[
+    'title' => 'Finish homework',
+    'due' => 'today',
+    'assigned_to' => 'Sam',
+    'completed' => false,
+];
 
-    public static function start(...$params)
-    {
-        return new static(...$params);
-    }
-
-    public function name()
-    {
-        return $this->name;
-    }
-    public function members()
-    {
-        return $this->members;
-    }
-    public function add($name)
-    {
-        $this->members[] = $name;
-    }
-    public function cancel()
-    {
-        //
-    }
-}
-
-class member
-{
-    protected $name;
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
-}
-
-$acme = Team::start('Sam', [
-    new member('SamFriend'),
-    new member('SamFriend2'),
-    new member('SamFriend3'),
-]);
-
-var_dump($acme->members());
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sam test</title>
+    <style>
+    /* header {
+        background: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+    } */
+    </style>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-800 text-gray-900">
+    <header class="container mx-auto text-center bg-orange-500 p-2 mt-10 rounded-lg">
+        <h1 class="text-4xl font-bold mb-5"> Testin with PHP</h1>
+        <table class="table-auto w-full">
+            <tr>
+                <?php foreach($task as $name => $val) : ?>
+                <th class="px-4 py-2"> <?= ucwords($name); ?></th>
+                <?php endforeach ?>
+            </tr>
+            <tr>
+                <td>
+                    <?= $task['title']; ?>
+                </td>
+                <td>
+                    <?= $task['due']; ?>
+                </td>
+                <td>
+                    <?= $task['assigned_to']; ?>
+                </td>
+                <td>
+                    <?= $task['completed'] ? 'Completed':'Not completed'; ?>
+                </td>
+            </table>
+    </header>
+</body>
+</html>
