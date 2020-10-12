df5d68829024a6d6487e171a6e86247440f68a55
Samk13 <samarbid13@gmail.com>
2020-10-12 02:22:11 +0200

    autoload with composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,11 +2,6 @@
 
 $app = [];
 
-require  'core/router/router.php';
-require  'core/database/Connections.php';
-require  'core/database/QueryBuilder.php';
-require  'core/database/Request.php';
-
 $app['config'] = require "./config.php";
 
 $app['database'] = new QueryBuilder(
